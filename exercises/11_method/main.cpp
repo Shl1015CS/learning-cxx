3dabeb7b7467d14df06fe4714cbf021a54e1a16d
Shl1015CS <1665105642@qq.com>
2024-12-18 18:59:15 +0800

    first

--- exercises/11_method/main.cpp
+++ exercises/11_method/main.cpp
@@ -6,9 +6,16 @@ struct Fibonacci {
 
     // TODO: 实现正确的缓存优化斐波那契计算
     unsigned long long get(int i) {
-        for (; false; ++cached) {
-            cache[cached] = cache[cached - 1] + cache[cached - 2];
+        for (int n = cached; n <= i; ++n) {
+            if (n == 0) {
+                cache[n] = 0;
+            } else if (n == 1) {
+                cache[n] = 1;
+            } else {
+                cache[n] = cache[n - 1] + cache[n - 2];
+            }
         }
+        cached = i;
         return cache[i];
     }
 };
@@ -16,6 +23,9 @@ struct Fibonacci {
 int main(int argc, char **argv) {
     // TODO: 初始化缓存结构体，使计算正确
     Fibonacci fib;
+    fib.cached = 2;
+    fib.cache[0] = 0;
+    fib.cache[1] = 1;
     ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
     return 0;
